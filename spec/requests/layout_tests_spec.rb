41281815417b7cd95dca611e025474d76a3e5e0f
Vasiliy Ganichev <ganichevv@gmail.com>
2012-08-31 18:26:54 +0400

    Remove default index rails page

--- spec/requests/layout_tests_spec.rb
+++ spec/requests/layout_tests_spec.rb
@@ -1,11 +1,24 @@
 require 'spec_helper'
 
-describe "LayoutTests" do
-  describe "GET /layout_tests" do
-    it "works! (now write some real specs)" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      get layout_tests_path
-      response.status.should be(200)
-    end
+describe "LayoutLinks" do
+
+  it "should have a Home page at '/'" do
+    get '/'
+    response.should have_selector('title', :content => "Home")
+  end
+
+  it "should have a Contact page at '/contact'" do
+    get '/contact'
+    response.should have_selector('title', :content => "Contact")
+  end
+
+  it "should have an About page at '/about'" do
+    get '/about'
+    response.should have_selector('title', :content => "About")
+  end
+
+  it "should have a Help page at '/help'" do
+    get '/help'
+    response.should have_selector('title', :content => "Help")
   end
 end
